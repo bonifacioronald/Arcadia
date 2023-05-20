a5b577d46e3723862fbf8dad3f0788bc97d10f19
bonifacioronald <bonifacioronald99@gmail.com>
2023-05-20 11:20:36 +0800

    basic-layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,141 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import '../models/user.dart';
 import '../models/colors.dart' as custom_colors;
 
 class HomeScreen extends StatelessWidget {
   static const routeName = "/home";
-  const HomeScreen({super.key});
+
+  User userDefaultData = User("Dylan Eu", 230, 23);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: custom_colors.backgroundPrimary,
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  custom_colors.backgroundPrimary.withOpacity(0.8),
+                  custom_colors.backgroundPrimary,
+                ],
+              )),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).padding.top + 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 52,
+                        height: 52,
+                        decoration: BoxDecoration(
+                            color: custom_colors.secondary,
+                            borderRadius: BorderRadius.circular(12)),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.menu_rounded,
+                          size: 36,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    'Hello,',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    userDefaultData.name,
+                    style: TextStyle(
+                        fontSize: 44,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${userDefaultData.totalFocusTime.toString()} h',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'Total Focused Time',
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${userDefaultData.totalSession.toString()}',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'Successful Sessions',
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: custom_colors.backgroundPrimary,
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
